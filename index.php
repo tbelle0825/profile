8baddc0f7a290768b15acfb16a36af90fa213b71
tbelle0825 <tbelle0825@gmail.com>
2018-06-13 14:29:08 +0900

    index update
    index update

---
+++ index.php
@@ -0,0 +1,461 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta name="viewport" content="width=960">
+<meta property="og:description" content="메인">
+<meta property="og:title" content="[LJH portfolio]">
+<meta property="og:url" content="2014/main.html">
+<title>LJH</title>
+<link rel="stylesheet" type="text/css" href="pfcss/nw-style.css">
+<link rel="stylesheet" type="text/css" href="pfcss/font_main.css">
+<script type="text/javascript" charset="utf-8" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
+<body>
+<div id="wrap_main">
+
+	<?php
+		include $_SERVER['DOCUMENT_ROOT'].'/include/header.php';
+	?>
+    <div class="nav" style="padding-top:0px">
+    	<!-- 다음, 이전 버튼 활성화시 class="on"이 추가되어 함. <span>태그는 자동으로 노출/제외 -->
+    	<h2 class="blind">현재 리스트위치</h2>
+        <span class="num" id="num"><strong>01</strong> / 07</span>
+        <h2 class="blind">리스트 이동 네비게이션</h2>
+        <a href="#" class="prev on">이전 항목으로 이동</a>
+        <span class="prev">이전 항목이 없습니다.</span>
+    	<a href="#" class="next on">다음 항목으로 이동</a>
+    	<span class="next">다음 항목이 없습니다.</span>
+    </div>
+
+	<!-- container -->
+	<div id="container">
+		<!-- content -->
+		<div id="content">
+        	<ul class="main_lst" style="width:100%;height:100%;overflow:hidden">
+        		<li id="main_tab1" style="width:100%;height:100%" >
+
+				   <img src="pfimg/bg_main6.jpg" width="100%">
+
+					<a href="mobileweb_new.html" class="main_box con1">
+						<h2>
+						Design and<br>cording<br>
+						<em>모바일웹</em>
+						</h2>
+						<p>
+						&nbsp;<br>
+						모바일 e-learning을 위한<br>
+						smart learning<br>
+						hybrid app
+						</p>
+						<span>자세히보기</span>
+					</a>
+				</li>
+
+	        	<li id="main_tab2" style="width:100%;height:0%;" >
+
+	                <img src="pfimg/2014/bg_main.gif" width="100%">
+
+	                <a href="etc/etcmain_new.html" class="main_box con2">
+	                	<h2>
+	                    Design and<br>cording<br>
+	                    <em>website</em>
+	                    </h2>
+	                    <p>
+	                    &nbsp;<br>
+						웹보여지는<br>
+						website design<br>
+						concept 시안들
+	                    </p>
+	                	<span>자세히보기</span>
+	                </a>
+	            </li>
+	            <li id="main_tab3" style="width:100%;height:0%;">
+					<div class="bg_main_old"></div>
+	                <a href="mobileweb.html#mobile-ui" class="main_box">
+	                	<h2>
+	                    vector<br>
+	                    icon design<br>
+	                    <em>UI design</em>
+	                    </h2>
+	                    <p>
+	                    아이콘 디자인,<br>
+	                    스마트폰앱용 아이콘디자인<br>
+	                    <br>
+	                    </p>
+	                	<span>자세히보기</span>
+	                </a>
+	            </li>
+	            <li id="main_tab4" style="width:100%;height:0%;">
+					<div class="bg_main_nanum move">
+	                	<div class="h_move"><div class="pt_no1"></div></div>
+	                	<div class="h_move"><div class="pt_no2"></div></div>
+	                	<div class="h_move"><div class="pt_no3"></div></div>
+	                	<div class="h_move"><div class="pt_no4"></div></div>
+	                	<div class="h_move"><div class="pt_no5"></div></div>
+	                	<div class="h_move"><div class="pt_no6"></div></div>
+	                	<div class="h_move"><div class="pt_no7"></div></div>
+	                	<div class="h_move"><div class="pt_no8"></div></div>
+	                	<div class="h_move"><div class="pt_no10"></div></div>
+	                	<div class="h_move"><div class="pt_no11"></div></div>
+	                	<div class="h_move"><div class="pt_no12"></div></div>
+	                	<div class="h_move"><div class="pt_no13"></div></div>
+	                	<div class="h_move"><div class="pt_no14"></div></div>
+	                	<div class="h_move"><div class="pt_no15"></div></div>
+	                	<div class="h_move"><div class="pt_no16"></div></div>
+	                	<div class="h_move"><div class="pt_no17"></div></div>
+	                	<div class="h_move"><div class="pt_no18"></div></div>
+	                	<div class="h_move"><div class="pt_no19"></div></div>
+	                	<div class="h_move"><div class="pt_no20"></div></div>
+	                	<div class="h_move"><div class="pt_no21"></div></div>
+	                	<div class="h_move"><div class="pt_no22"></div></div>
+	                	<div class="h_move"><div class="pt_no23"></div></div>
+	                	<div class="h_move"><div class="pt_no24"></div></div>
+	                	<div class="h_move"><div class="pt_no25"></div></div>
+	                	<div class="h_move"><div class="pt_no26"></div></div>
+	                	<div class="h_move"><div class="pt_no27"></div></div>
+	                	<div class="h_move"><div class="pt_no28"></div></div>
+	                	<div class="h_move"><div class="pt_no29"></div></div>
+	                	<div class="h_move"><div class="pt_no30"></div></div>
+	                	<div class="h_move"><div class="pt_no31"></div></div>
+	                	<div class="h_move"><div class="pt_no32"></div></div>
+	                </div>
+	                <a href="event.html" class="main_box">
+	                	<h2>
+	                    landing<br>
+	                    event<br>
+	                    <em>design<i class="num">2015</i></em>
+	                    </h2>
+	                    <p>
+	                    <br>
+	                    <br>
+	                    &nbsp;<br>
+	                    </p>
+	                	<span>자세히보기</span>
+	                </a>
+	            </li>
+	            <li id="main_tab5" style="width:100%;height:0%" >
+
+	                <img src="pfimg/2014/bg_main5.gif" width="100%">
+	                <a href="cording.html" class="main_box con5">
+	                	<h2>
+	                    html5 + CSS3<br>
+	                    + jquery<br>
+	                    <em>publishing <span></span></em>
+	                    </h2>
+	                    <p class="v2">
+	                    지금은 퍼블리싱에<br>
+	                    더 애정을 가지고<br>
+	                    sass, javascript study에<br>
+	                    노력하는 중...
+	                    </p>
+	                	<span>자세히보기</span>
+	                </a>
+	            </li>
+	            <li id="main_tab6" style="width:100%;height:0%" >
+
+	                <img src="pfimg/2014/bg_main3.jpg" width="100%">
+	                <a href="luncher.html" class="main_box con3">
+	                	<h2>
+	                    SK<br>
+	                    런처 플래닛<br>
+	                    <em>UI design</em>
+	                    </h2>
+	                    <p class="v2">
+	                    2013 SK런처 <br>
+	                    UI 디자인<br>
+	                    결과물들
+	                    </p>
+	                	<span>자세히보기</span>
+	                </a>
+	            </li>
+	            <li id="main_tab7" style="width:100%;height:0%" >
+
+	           		<img src="pfimg/2014/bg_main4.jpg" width="100%">
+	                <a href="#" target="_blank" class="main_box" title="준비중입니다.">
+	                    <h2>
+	                    Lee Jung Hyun<br>
+	                    about me<br>
+	                    <em>resume</em>
+	                    </h2>
+	                    <p>
+	                    Seoul Korea<br>
+	                    e-mail: tbelle@naver.com<br>
+	                    phone: +82-010-8744-2581(Korea-stop)<br> 
+	                    &nbsp;
+	                    </p>
+	                    <span>자세히보기</span>
+	                </a>
+	            </li>
+	        </ul>
+
+	    </div>
+    </div>
+    <div id="footer" style="position:fixed">
+
+    	<!--a href="#" class="inform">SNS 공유하기</a -->
+        <span>Lee Jung Hyun's website</span>
+
+    </div>
+</div>
+
+<script type="text/javascript" charset="utf-8" src="pfjs/jquery.mousewheel.js"></script>
+<script type="text/javascript" charset="utf-8" src="pfjs/jquery.backgroundSize.js"></script>
+<script type="text/javascript" charset="utf-8" src="pfjs/scrap-1.0.05.js"></script>
+<script type="text/javascript" charset="utf-8" src="pfjs/api/lcslog-0.4.13.js"></script>
+<!-- <script type="text/javascript" charset="utf-8" src="pfjs/api/javascript/nhn/nclicks/clickcr.js"></script> -->
+<script type="text/javascript" charset="utf-8" src="pfjs/hangeul2014_common.js"></script>
+
+<script type="text/javascript">
+	var current = 0;
+
+	$(document).ready(function(){
+		resize();
+
+		var timer = setInterval(function () {
+			showNextSection();
+	    }, 4000);
+
+		//상위 메뉴
+		$(".gnb_on").click(function() {
+			if(isGnbClose()) {
+				$("#header").animate({top: 0}, 500);
+			} else {
+				$("#header").animate({top: -60}, 500);
+			}
+
+			return false;
+		});
+
+		// $("#gnblist li").on('click', function() {
+
+		// 	$("#gnblist li").removeClass("on");
+		// 	$(this).addClass("on");
+
+		// });
+
+		// $("#gnblist li").on('click', function() {
+
+		// 	$("#gnblist li").removeClass("on");
+		// 	$(this).addClass("on");
+		// 	// return false;
+		// });
+
+		//sns 버튼
+		$(".inform").click(function() {
+			if($(".inform").hasClass("on")) {
+				$(".inform").removeClass("on");
+			} else {
+				$(".inform").addClass("on");
+			}
+
+			return false;
+		});
+
+		//휠 이벤트
+		$('.main_lst li').mousewheel(function(event, delta) {
+			clearInterval(timer);
+
+	        if(delta == -1) {
+	        	showNextSection();
+	        } else {
+	        	showPrevSection();
+	        }
+	        return false; // prevent default
+	    });
+
+		//키보드 이벤트
+		$(document).bind('keydown', function (e){
+			clearInterval(timer);
+
+			if(e.which == 38) {
+				showPrevSection();
+			} else if(e.which == 40) {
+				showNextSection();
+			}
+		});
+
+		//이전 다음 버튼
+		$(".prev").click(function(e) {
+			e.preventDefault();
+			clearInterval(timer);
+			showPrevSection();
+		});
+		$(".next").click(function(e) {
+			e.preventDefault();
+			clearInterval(timer);
+			showNextSection();
+		});
+
+		//리사이즈
+		$(window).resize(function() {
+			resize();
+		});
+	});
+
+	//li의 id어트리뷰트에서 마지막 숫자로 되어있는 부분만 가져와서 제어
+	function showPrevSection() {
+		var index = 0;
+		var prevIndex = 0;
+		var animateYn = false;
+
+		$(".main_lst li").each(function(e) {
+			if($(this).height() > 0) {
+				index = Number($(this).attr("id").slice(-1));
+				if(e == 0) {
+					prevIndex = Number($(".main_lst li:last").attr("id").slice(-1));
+				} else {
+					prevIndex = Number($(this).prev().attr("id").slice(-1));
+				}
+
+				if($(this).is(":animated")) {
+					animateYn = true;
+				}
+			}
+		});
+
+		if(animateYn) {
+			return false;
+		}
+
+		var curIndex = index;
+
+		if(curIndex == 1) {
+			$("#main_tab" + prevIndex).css("height", "0%");
+			$(".main_lst li:last").prependTo(".main_lst");
+			$("#main_tab" + prevIndex).animate({
+			    height: "100%"
+			}, 1000, function() {
+				$("#main_tab" + curIndex).css("height", "0%");
+				$(".main_lst li:first").appendTo(".main_lst");
+
+				setPaging();
+			});
+		} else {
+			$("#main_tab" + prevIndex).css("height", "0%");
+			$("#main_tab" + prevIndex).animate({
+			    height: "100%"
+			}, 1000, function() {
+				$("#main_tab" + curIndex).css("height", "0%");
+				setPaging();
+			});
+		}
+
+		return false;
+	}
+
+	//li의 id어트리뷰트에서 마지막 숫자로 되어있는 부분만 가져와서 제어
+	function showNextSection() {
+		var index = 0;
+		var nextIndex = 0;
+		var animateYn = false;
+
+		$(".main_lst li").each(function(e) {
+			if($(this).height() > 0) {
+				index = Number($(this).attr("id").slice(-1));
+				if(e < ($(".main_lst li").length -1)) {
+					nextIndex = Number($(this).next().attr("id").slice(-1));
+				} else {
+					nextIndex = Number($(".main_lst li:first").attr("id").slice(-1));
+				}
+
+				if($(this).is(":animated")) {
+					animateYn = true;
+				}
+			}
+
+		});
+
+		if(animateYn) {
+			return false;
+		}
+
+		var curIndex = index;
+
+		if(curIndex == $(".main_lst li").length) {
+			$(".main_lst li:first").appendTo(".main_lst");
+			$("#main_tab" + nextIndex).css("height", "100%");
+			$("#main_tab" + curIndex).animate({
+			    height: "0%"
+			}, 1000, function() {
+				$(".main_lst li:last").prependTo(".main_lst");
+				setPaging();
+			});
+		} else {
+
+			$("#main_tab" + nextIndex).css("height", "100%");
+			$("#main_tab" + curIndex).animate({
+			    height: "0%"
+			}, 1000, function() {
+				setPaging();
+			});
+		}
+
+
+		return false;
+	}
+
+	function setPaging() {
+		var index = 1;
+		$(".main_lst li").each(function(e) {
+			if($(this).height() > 0) {
+				index = Number($(this).attr("id").slice(-1));
+			}
+		});
+
+		/* if(index == 0) {
+			$(".prev").removeClass("on");
+		} else if(index == 4) {
+			$(".next").removeClass("on");
+		} else {
+			$(".prev, .next").addClass("on");
+		}*/
+		if(index == 1 || index == 2 || index == 3 || index == 4 || index == 6 || index == 7) {
+			$(".nav").removeClass("v2");
+			$("#header h1").eq(0).hide();
+			$("#header h1").eq(1).show();
+		} else {
+			$(".nav").addClass("v2");
+			$("#header h1").eq(1).hide();
+			$("#header h1").eq(0).show();
+		}
+
+		$("#num").html("<strong>0" + index + "</strong> / 07</span>");
+	}
+
+	var backgroundRatio = 864/495;
+	function resize() {
+		$(".main_lst li").each(function(e) {
+			var index = $(".main_lst li").index($(this));
+			if(index == 0 || index == 1 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7) {
+				var imgHeight = $(this).children("img").height();
+				var imgWidth = $(this).children("img").width();
+
+				if(imgHeight < $(window).height()) {
+					$(this).children("img").css("height", $(window).height());
+					$(this).children("img").css("width", $(window).height() * backgroundRatio);
+				}
+
+				if(imgWidth < $(window).width()) {
+					$(this).children("img").css("width", $(window).width());
+					$(this).children("img").css("height", $(window).width() / backgroundRatio);
+				}
+			}
+		});
+	}
+
+	function isGnbClose() {
+		if($("#header").css("top") == "-60px") {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// lcs_do();
+</script>
+</body>
+</html>
